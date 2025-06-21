5590dadb0f116cec459aa5de6024d59639e10ccc
karolgorka2424 <karol.gorka@microsoft.wsei.edu.pl>
2025-06-21 10:31:49 +0200

    lab 5 not working change/api

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
+  plugins: [
+    react(),
+    tailwindcss()  // Dodaj plugin Tailwind v4
+  ],
   server: {
     port: 5173,
     open: true
